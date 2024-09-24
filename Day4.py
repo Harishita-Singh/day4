2cecddb7806dd52e84e01cd535e4a2131d8a7aad
Harishita Singh <harishitasinghmahua@gmail.com>
2024-09-24 13:36:37 +0530

    Forth time pushed

---
+++ Day4.py
@@ -0,0 +1,239 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import numpy
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    import numpy
+ModuleNotFoundError: No module named 'numpy'
+print("Hello World")
+Hello World
+2+3
+5
+2**3
+8
+"MCE"*3
+'MCEMCEMCE'
+"NCE "*3
+'NCE NCE NCE '
+'NCE '*3
+'NCE NCE NCE '
+x=5
+type(5)
+<class 'int'>
+x="Roshni"
+type(x)
+<class 'str'>
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Hello
+World
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Traceback (most recent call last):
+  File "C:/Users/HP/Gecvaishali/Day4.py", line 1, in <module>
+    prin("Hello")
+NameError: name 'prin' is not defined. Did you mean: 'print'?
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Hello
+Traceback (most recent call last):
+  File "C:/Users/HP/Gecvaishali/shellpy.py", line 2, in <module>
+    prin("World")
+NameError: name 'prin' is not defined. Did you mean: 'print'?
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Hello
+World
+x=5
+name= Roshni
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    name= Roshni
+NameError: name 'Roshni' is not defined
+name="Roshni"
+first_name="Roshni"
+first_name
+'Roshni'
+print("Hello World")
+Hello World
+x=7
+print(x)
+7
+x=input("Enter a nunmber:")
+Enter a nunmber:6
+print(x)
+6
+x,y=input("Enter two number:").sep()
+Enter two number:5
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    x,y=input("Enter two number:").sep()
+AttributeError: 'str' object has no attribute 'sep'
+x=input
+x=input("Enter a number")
+Enter a number
+x=input("Enter a number:")
+Enter a number: 6
+type(x)
+<class 'str'>
+x=int(input("Enter a number:"))
+Enter a number:6
+type(s)
+Traceback (most recent call last):
+  File "<pyshell#28>", line 1, in <module>
+    type(s)
+NameError: name 's' is not defined
+x=int(input("Enter a number:"))
+Enter a number: 7
+type(7)
+<class 'int'>
+y=float(input("Enter CGPA:"))
+Enter CGPA:6.7
+type(y)
+<class 'float'>
+x,y=input("Enter two numbers :").split()
+Enter two numbers : 6 7
+x=int(x)
+y=int(y)
+x+y
+13
+x,y=input("Enter two numbers:").split()
+Enter two numbers: 6 7
+x+y
+'67'
+print("Hello \n World")
+Hello 
+ World
+print("Hello\nWorld")
+Hello
+World
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+HelloWorld
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Hello                   World
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Hello World Welcome to Workshop
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Roshni,Simran,Harishita  are fiends.
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Roshni,Simran,Harishitaare fiends.
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Roshni,Simran,Harishita are fiends.
+
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Roshni#Simran#Harishita are fiends.
+7**2
+49
+4//2
+2
+4.5//2
+2.0
+5//2
+2
+x=6
+x**=2
+x
+36
+x=["Roshni","Loctus"]
+print(' Is value Present?', "Roshni" in x)
+ Is value Present? True
+x=["Roshni","Loctus"]
+print("{Roshni" in x)
+False
+x=["Roshni","Loctus"]
+ptint("Roshni"in x)
+Traceback (most recent call last):
+  File "<pyshell#53>", line 1, in <module>
+    ptint("Roshni"in x)
+NameError: name 'ptint' is not defined. Did you mean: 'print'?
+x=["Roshni","Loctus"]
+print("Roshni"in x)
+True
+print("Loctus" not in x)
+False
+a = ["Rose", "Lotus"]  
+b = ["Rose", "Lotus"]  
+c = a  
+print(a is c)
+SyntaxError: multiple statements found while compiling a single statement
+a = ["Rose", "Lotus"]  
+
+x=6
+y=7
+print(x is y)
+False
+y=x
+print(x is y)
+True
+x="Roshni"
+y=2
+print(x is y)
+False
+z="8"
+print(y is z)
+False
+# This is comment
+# This is 2nd comment
+# This is 3rd comment
+"""bnjsfbnjshfuisw
+bnvjvbnhfiu
+mknjmiojio
+njdoijie
+jnvdijhfuwfhyuwfjnvk
+jhsbfuwyhgfuwhjf9y76ew
+""
+... """
+'bnjsfbnjshfuisw\nbnvjvbnhfiu\nmknjmiojio\nnjdoijie\njnvdijhfuwfhyuwfjnvk\njhsbfuwyhgfuwhjf9y76ew\n""\n'
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Roshni#Simran#Harishita are fiends.
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+Roshni#Simran#Harishita are fiends.
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/shellpy.py ===============================================================
+>>> if(x=7);"""bnjsfbnjshfuisw
+... bnvjvbnhfiu
+... mknjmiojio
+... njdoijie
+... jnvdijhfuwfhyuwfjnvk
+... jhsbfuwyhgfuwhjf9y76ew
+... ""
+... """
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> if(x==7);
+SyntaxError: invalid syntax
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Traceback (most recent call last):
+  File "C:/Users/HP/Gecvaishali/Day4.py", line 1, in <module>
+    if(x==7):
+NameError: name 'x' is not defined
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Traceback (most recent call last):
+  File "C:/Users/HP/Gecvaishali/Day4.py", line 1, in <module>
+    if(x==7):
+NameError: name 'x' is not defined
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Hello
+Gec Vaishali
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Gec Vaishali
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Gec Vaishali
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Gec Vaishali
+>>> 
+================================================================ RESTART: C:/Users/HP/Gecvaishali/Day4.py ===============================================================
+Hello
